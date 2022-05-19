9446f06747081257e8f7b837dc0da0e3e6de51b7
Benchamon <andres.benjamin.godoy@gmail.com>
2022-05-19 11:52:30 -0500

    Exercise 3 done

--- III. Loading data from file/js/main.js
+++ III. Loading data from file/js/main.js
@@ -1,4 +1,30 @@
 /*
 *    main.js
 */
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 400)
+	.attr("height", 400)
+	.style("background-color", "cyan")
+
+d3.json("data/ages.json").then((data)=> {
+	data.forEach((d)=>{
+
+		d.age = +d.age;
+
+	});
+
+	var circles = svg.selectAll().data(data);
+
+	circles.enter()
+		.append("circle")
+		.attr("cx",(d,i)=>(i*50)+35)
+		.attr("cy", 200)
+		.attr("r", (d)=>d.age)
+		.attr("fill",(d)=>{if(d.age>10){return "blue"}else{return"red"};})
+})
+.catch((error)=>{console.log(error);});
+
+
+
+
 
