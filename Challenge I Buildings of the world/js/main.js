e4db597365d41811e5760ba2c8046f26756fafdd
Benchamon <andres.benjamin.godoy@gmail.com>
2022-05-19 12:18:54 -0500

    Challenge 1 done

--- Challenge I Buildings of the world/js/main.js
+++ Challenge I Buildings of the world/js/main.js
@@ -2,3 +2,30 @@
 *    main.js
 */
 
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 800)
+	.attr("height", 900)
+	.style("background-color", "grey")
+	.style("stroke","white");
+
+d3.json("data/buildings.json").then((data)=> {
+	data.forEach((d)=>{
+
+		d.height = +d.height;
+
+	});
+	console.log(data);
+	var rectangles = svg.selectAll("rectangle").data(data);
+
+	rectangles.enter()
+	.append("rect")
+	.attr("x",(d,i)=>(i*50)+10)
+	.attr("y", (d)=>(900-d.height)+30)
+	.attr("width",20)
+	.attr("height", (d)=>d.height)
+	.attr("fill","blue");
+})
+.catch((error)=>{console.log(error);});
+
+
+
